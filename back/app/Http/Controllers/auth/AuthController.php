9a2fb78418129c070c7c4f5715ae9b717a0c6816
Jonathan0148 <bohorquezvillamiljonathan@gmail.com>
2024-03-15 12:18:39 -0500

    first commit

--- back/app/Http/Controllers/auth/AuthController.php
+++ back/app/Http/Controllers/auth/AuthController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\auth;
 
 use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
-use App\Models\settings\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
